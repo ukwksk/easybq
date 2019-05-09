d8b536d32c156d2763d258179b44824acebe9fe7
ukwksk <ukwksk@gmail.com>
2019-05-09 23:07:18 +0900

    v0.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import find_packages, setup
 
-version = "0.2.1"
+version = "0.2.2"
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
     readme = f.read()
